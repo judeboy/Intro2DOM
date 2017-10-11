bac8c2266fdbe188c36b93321187ce0d08e261af
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-10-11 12:00:22 -0600

    completed

--- main.js
+++ main.js
@@ -6,12 +6,21 @@ function changePic (){
 
 //Change the headings text color to blue
 //HINT: it's a class!
-
+var headings = document.getElementsByClassName('headings');
+  for(var i = 0; i < headings.length; i++){
+    headings[i].style.color = 'blue';
+  }
 
 //Change the button's background-color to green
+var button = document.getElementById('button');
+  console.log(button);
+  button.style.backgroundColor = 'green';
 
 
 //Change the paragraph font to 'Tahona'
-
+var fonts = document.getElementsByClassName('paragraph');
+  fonts[0].style.fontFamily = "Arial";
 
 //Change the entire documents background color to yellow
+var background = document.getElementsByTagName('body');
+  background[0].style.backgroundColor = "yellow";
